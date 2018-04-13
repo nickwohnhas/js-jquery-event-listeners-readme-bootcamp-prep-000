97fbb0dbe6adea328083e73a5f527b9ae4716e25
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-13 00:17:14 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,7 +10,9 @@ function frameIt(){
 }
 function pressIt(){
   $('#typing').on('keydown',function(key){
-    if (key.which ==  )
+    if (key.which == 71 ){
+      alert(`G has been pressed.`)
+    }
   })
 }
 
